6bc5e0453237684f939e6e34927930462b517061
Manav <manavjoshi313@gmail.com>
2026-02-12 23:03:52 +0530

    [UPDATE] users module , changed admin.py

--- Webapp/users/admin.py
+++ Webapp/users/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import UserProfile
-# Register your models here.
 
-admin.site.register(UserProfile)
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "role",
+        "badge_number",
+        "department",
+        "created_at",
+    )
+
+    list_filter = ("role", "department")
+    search_fields = ("user__username", "badge_number")
