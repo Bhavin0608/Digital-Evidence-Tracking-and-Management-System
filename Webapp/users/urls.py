35bc5cd7a25714ef3982e6fde7fc8edf758821f6
Bhavin0608 <bhavinmakwana2006@gmail.com>
2026-02-15 01:05:20 +0530

    [UPDATE] Login UI update added new urls

--- Webapp/users/urls.py
+++ Webapp/users/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path("login/", views.login_view, name="login"),
-    path("home/", views.home, name="home"),
+    path("dashboard/so/", views.so_dashboard, name="so_dashboard"),
+    path("dashboard/investigator/", views.investigator_dashboard, name="investigator_dashboard"),
+    path("dashboard/auditor/", views.auditor_dashboard, name="auditor_dashboard"),
+    path("logout/", views.logout_view, name="logout"),
 ]
--- Webapp/users/views.py
+++ Webapp/users/views.py
@@ -1,16 +1,43 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
+from django.contrib.auth import logout
 
 from users.models import UserProfile
 
+def logout_view(request):
+    logout(request)          # destroys session
+    return redirect("login")  # redirect to login page
+
+@never_cache
 def root_redirect(request): # This is used to use root http://127.0.0.1:8000/ not explicit write home/ or login/ etc.
 
     if request.user.is_authenticated:
-        return redirect("home")   # logged-in landing page
-    else:
-        return redirect("login")  # login page
 
+        user = request.user
+
+        # 🔹 Check if profile exists
+        if not hasattr(user, "profile"):
+            return redirect("/admin/")
+
+        role = user.profile.role
+
+        if role == "SENIOR_OFFICER":
+            return redirect("so_dashboard")
+
+        elif role == "INVESTIGATOR":
+            return redirect("investigator_dashboard")
+
+        elif role == "AUDITOR":
+            return redirect("auditor_dashboard")
+
+        else:
+            return redirect("login")
+
+    else:
+        return redirect("login")
+@never_cache
 def login_view(request):
 
     if request.method == "POST":
@@ -26,22 +53,28 @@ def login_view(request):
 
         if user is not None:
 
+            # 🔹 Check if profile exists # if admin trys to login in then it deny
+            if not hasattr(user, "profile"):
+                return render(
+                    request,
+                    "users/login.html",
+                    {"error": "No authorized user exists."}
+                )
+
             login(request, user)
 
             # 🔹 Get role
-            # role = user.profile.role
+            role = user.profile.role
 
             # # 🔹 Redirect based on role
-            # if role == "SENIOR_OFFICER":
-            #     return redirect("so_dashboard")
+            if role == "SENIOR_OFFICER":
+                return redirect("so_dashboard")
 
-            # elif role == "INVESTIGATOR":
-            #     return redirect("investigator_dashboard")
+            elif role == "INVESTIGATOR":
+                return redirect("investigator_dashboard")
 
-            # elif role == "AUDITOR":
-            #     return redirect("auditor_dashboard")
-            return redirect("home")
-
+            elif role == "AUDITOR":
+                return redirect("auditor_dashboard")
 
         else:
             return render(
@@ -52,6 +85,17 @@ def login_view(request):
 
     return render(request, "users/login.html")
 
+@never_cache
 @login_required
-def home(request):
-    return render(request, "users/home.html")
+def so_dashboard(request):
+    return render(request, "users/so_dashboard.html")
+
+@never_cache
+@login_required
+def investigator_dashboard(request):
+    return render(request, "users/investigator_dashboard.html")
+
+@never_cache
+@login_required
+def auditor_dashboard(request):
+    return render(request, "users/auditor_dashboard.html")
